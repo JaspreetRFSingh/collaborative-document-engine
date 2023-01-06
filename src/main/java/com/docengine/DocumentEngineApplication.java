00f24fc9103a91c190404dc6ef7347a54464a29d
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2023-01-06 11:00:00 +0530

    Add main application class

---
+++ src/main/java/com/docengine/DocumentEngineApplication.java
@@ -0,0 +1,9 @@
+package com.docengine;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DocumentEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentEngineApplication.class, args);
+    }
+}
