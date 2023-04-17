6f0d0b29c74973875e241f1d4c410dfa2dbe46c4
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2023-04-17 14:30:00 +0530

    Configure Redis template

---
+++ src/main/java/com/docengine/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.docengine.config;
+import org.springframework.context.annotation.*;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.core.*;
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        return template;
+    }
+}
